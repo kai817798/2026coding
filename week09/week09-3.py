3bef7cc724099b1dce63c95c767f9d79acbb4f70
kai817798 <hyk20060817@gmail.com>
2026-04-23 15:44:00 +0800

    week09

---
+++ week09/week09-3.py
@@ -0,0 +1,13 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head == None or head.next == None:
+            return head
+        head2 = head.next
+        ans = self.reverselist(head.next)
+        head2.next,head.next = head,None
+        return ans
